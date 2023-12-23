023e7c9efe3332a941ec502ee9b1528a53466b25
abdullhakim.sami <abdullhakim.sami@gmail.com>
2023-12-23 22:29:50 +0300

    choosing open props for css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import markdoc from "@astrojs/markdoc";
+
 // https://astro.build/config
 export default defineConfig({
-  
-    site: 'https://abdullhakim-sami.github.io',
-    base: '/sanabel-al-firdaws-astro',
+  site: 'https://abdullhakim-sami.github.io',
+  base: '/sanabel-al-firdaws-astro',
+  integrations: [markdoc()]
 });
