d542027ac3f1cb327dfd7b5cadc520ae6b1f103c
abdullhakim.sami <abdullhakim.sami@gmail.com>
2023-12-23 07:41:29 +0300

    test

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  
+    site: 'https://abdullhakim-sami.github.io',
+    base: '/sanabel-al-firdaws-astro',
+});
